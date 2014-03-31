c3ea3af555efb0e533c1b63545225beb4426e83e
Maxim Krivodaev <maxim.krivodaev@kbor.ru>
2014-03-31 18:16:16 +0400

    Add some tests

--- js/tests.js
+++ js/tests.js
@@ -20,8 +20,9 @@ window.onload = function () {
         ok(document.getElementById('map'), 'Document has container');
         ok(document.getElementById('map').getElementsByTagName('ymaps'), 'Container has map');
         
-        expect( 2 );
+        expect( 14 );
         setTimeout(function(){
+            
             equal(
                 Math.round(creator.map.getCenter()[0] * error) / error, 
                 Math.round(55.75367599999372 * error) / error, 
@@ -32,40 +33,38 @@ window.onload = function () {
                 Math.round(37.61989899999996 * error) / error, 
                 'Longtitude correct'
             );
-        }, 1000);
-        
-        expect( 9 );
-        setTimeout(function(){
+
             var i = 0
             creator.map.geoObjects.each(function(obj){
+                var coord = obj.geometry.getCoordinates();
                 if (i == 0) {
-                    var coord = obj.geometry.getCoordinates();
-
                     var x = Math.floor((coord[1][0] - coord[0][0]) * error) / error;
                     var y = Math.floor((coord[1][1] - coord[0][1]) * error) / error;
                     equal(x, y, 'Sides equal');
                     equal(myMath.calcAngle([0, 1], [0, 0], [1, 0]), 90, 'Math correct');
                     var sw_angle = myMath.calcAngle(
-                        [
-                            coord[0][0], coord[1][1]
-                        ], 
+                        [coord[0][0], coord[1][1]], 
                         coord[0], 
-                        [
-                            coord[1][0], coord[0][1]
-                        ]
+                        [coord[1][0], coord[0][1]]
                     );
                     var ne_angle = myMath.calcAngle(
-                        [
-                            coord[0][0], coord[1][1]
-                        ],
+                        [coord[0][0], coord[1][1]],
                         coord[1],
-                        [
-                            coord[1][0], coord[0][1]
-                        ]
+                        [coord[1][0], coord[0][1]]
                     );
                     equal(ne_angle, 90, 'NE rect angle 90 degrees');
                     equal(sw_angle, 90, 'SW rect angle 90 degrees');
                     equal(ne_angle, sw_angle,  'Rect angles equal');
+                } else if (i == 1) {
+                    var a = coord[0][0];
+                    var b = coord[0][1];
+                    var c = coord[0][2];
+                    var abc = Math.round(myMath.calcAngle(a, b, c) * error) / error;
+                    var bca = Math.round(myMath.calcAngle(b, c, a) * error) / error;
+                    var cab = Math.round(myMath.calcAngle(c, a, b) * error)/ error;
+                    console.log(abc, bca, cab);
+                    equal(cab, abc);
+                    equal(abc + bca + cab, 180);
                 }
                 i++;
             });
